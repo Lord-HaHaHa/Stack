1320751ca8f1d8cd67a2d46bee6b7840a06058d6
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2020-10-23 11:51:58 +0200

    Projektdateien hinzufügen.

---
+++ Stack/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stack
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Stack stack = new Stack();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPush_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtPush.Text, out int value))
+                stack.Push(value);
+            txtPush.Clear();
+            UpdateStackFill();
+        }
+        private void btnPop_Click(object sender, RoutedEventArgs e)
+        {
+            txtPop.Clear();
+            txtPop.Text = stack.Pop().ToString();
+            UpdateStackFill();
+        }
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtStackSize.Text = slidMaxStackSize.Value.ToString("");
+            stack.MaxLength = (int)slidMaxStackSize.Value;
+            slidMaxStackSize.Minimum = stack.GetFillStatus();
+            UpdateStackFill();    
+        }
+
+        private void UpdateStackFill()
+        {
+            txtStackFill.Text = stack.GetFillStatus().ToString();
+            progbarStackFillStatus.Maximum = stack.MaxLength;
+            progbarStackFillStatus.Value = stack.GetFillStatus();
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public partial class MainWindow
+    {
+        public class Stack
+        {
+            // Attribute
+            private int _MaxLength;
+            private List<int> StackElements;
+
+            // Eigenschaften
+            public int MaxLength
+            {
+                get => _MaxLength;
+                set
+                {
+                    if(value >= 0)
+                    {
+                        _MaxLength = value;
+                    }
+                    else
+                        throw new ArgumentOutOfRangeException($"{nameof(value)} cant be lower than 0.");
+                }
+            }
+            
+            // Konstruktor
+            public Stack()
+            {
+                StackElements = new List<int>();
+                MaxLength = 1;
+            }
+
+            // Methoden
+            public void Push(int value)
+            {
+                if (StackElements.Count < MaxLength)
+                    StackElements.Insert(0,value);
+                else
+                    throw new ArgumentException("stack is allredy full");
+            }
+            public int Pop()
+            {
+                int value;
+                if (GetFillStatus() > 0)
+                {
+                    value = StackElements[0];
+                    StackElements.RemoveAt(0);
+                    return value;
+                }
+                throw new ArgumentException("stack is allredy empty");
+            }
+            public int GetFillStatus() => StackElements.Count;
+
+        }
+    }
+}
